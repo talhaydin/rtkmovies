cdf087ca595cbb3710896c9d330982d6423f62db
Talha Aydin <mt.aydin@outlook.com>
2022-07-21 13:04:23 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.scss';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import Footer from './components/Footer/Footer';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-    <h1>App</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
