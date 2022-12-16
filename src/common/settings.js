3a544b9a00adaa7766f060b00c211351d65f1630
talhaaydin <mt.aydin@outlook.com>
2022-12-16 18:40:58 +0300

    react-slick slider

---
+++ src/common/settings.js
@@ -0,0 +1,52 @@
+export var Settings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 4,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1600,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -3,6 +3,9 @@ import { useSelector } from "react-redux";
 import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
+import Slider from "react-slick";
+import { Settings } from "../../common/settings";
+
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
   const shows = useSelector(getAllShows);
@@ -15,7 +18,7 @@ const MovieListing = () => {
         <MovieCard key={index} data={movie} />
       ))
     ) : (
-      <div className="movies-error">
+      <div className="error">
         <h3>{movies.Error}</h3>
       </div>
     );
@@ -24,7 +27,7 @@ const MovieListing = () => {
     shows.Response === "True" ? (
       shows.Search.map((movie, index) => <MovieCard key={index} data={movie} />)
     ) : (
-      <div className="shows-error">
+      <div className="error">
         <h3>{shows.Error}</h3>
       </div>
     );
@@ -32,11 +35,16 @@ const MovieListing = () => {
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
-        <div className="movie-container">{renderMovies}</div>
+
+        <div className="movie-container">
+          <Slider {...Settings}>{renderMovies}</Slider>
+        </div>
       </div>
       <div className="show-list">
         <h2>Shows</h2>
-        <div className="movie-container">{renderShows}</div>
+        <div className="movie-container">
+          <Slider {...Settings}>{renderShows}</Slider>
+        </div>
       </div>
     </div>
   );
